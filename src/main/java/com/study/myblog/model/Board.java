3b0ab8f9a5cfb60a0ba6bbbdf9b741ff44af05fd
yenniesong <songye93@naver.com>
2024-01-24 04:21:52 +0900

    2rd commit user, board, reply 테이블 완.

--- src/main/java/com/study/myblog/model/Board.java
+++ src/main/java/com/study/myblog/model/Board.java
@@ -1,4 +1,42 @@
 package com.study.myblog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    private String content; // 섬머노트 라이브러리를 사용할 거임. <html> 태그가 섞여서 디자인이 되므로 용량이 커짐 -> @Lob 사용
+
+    @ColumnDefault("0")
+    private int count; // 조회수
+
+    @ManyToOne(fetch = FetchType.EAGER)  // many = Board, one = User
+    @JoinColumn(name = "userId")
+    private User user;  // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
+
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER)  // mappedBy 연관 관계의 주인이 아니다.(난 FK가 가 아니에요) DB에 컬럼을 만들지 마세요
+    private List<Reply> reply;
+
+    @CreationTimestamp
+    private Timestamp createDate;
 }
--- src/main/java/com/study/myblog/model/User.java
+++ src/main/java/com/study/myblog/model/User.java
@@ -1,7 +1,18 @@
 package com.study.myblog.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.*;
 import java.sql.Timestamp;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder    // 빌더 패턴
 @Entity // User 클래스가 mysql에 자동으로 테이블이 생성된다.
 public class User {
     @Id // Primary key
@@ -9,7 +20,13 @@ public class User {
     private long id; // 시퀀스, auto_increment
     @Column(nullable = false, length = 30)
     private String username; // 아이디
+    @Column(nullable = false, length = 100)// 넉넉하게 주는 이유는 -> 해시로 변경해서 비밀번호를 암호화 할 것이라서
     private String password;
+    @Column(nullable = false, length = 50)
     private String email;
+
+    @ColumnDefault("'user'")
+    private String role;    // enum을 쓰는게 좋음. 도메인 설정을 할 수 있게 해줌
+    @CreationTimestamp  // 시간이 자동으로 입력된다.
     private Timestamp createDate;
 }
