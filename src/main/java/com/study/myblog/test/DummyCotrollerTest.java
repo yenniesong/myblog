fd648a991c3b59eb34e7bd97e542a29bd70f26a8
yenniesong <songye93@naver.com>
2024-01-26 04:20:56 +0900

    4rd commit user list 출력, list paging, update 까지 완.

--- src/main/java/com/study/myblog/test/DummyCotrollerTest.java
+++ src/main/java/com/study/myblog/test/DummyCotrollerTest.java
@@ -4,17 +4,62 @@ import com.study.myblog.model.RoleType;
 import com.study.myblog.model.User;
 import com.study.myblog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 @RestController // html 파일이 아니라 Data를 리턴해주는 controller = RestController
 public class DummyCotrollerTest {
     @Autowired  // 의존성 주입
     private UserRepository userRepository;  // DummyCotrollerTest가 메모리에 띄어줄 때 같이 UserRepository도 띄어준다.
+
+    @Transactional
+    @PutMapping("/dummy/user/{id}") // detail 메소드와 주소가 동일한데 괜찮은 이유는 PutMapping이라서 ! 상관 없음
+    public User updateUser(@PathVariable long id, @RequestBody User requestUser){    // email, password
+        // json 데이터를 요청 => Java Object(MessageConverter의 Jackson 라이브러리가 변환해서 받아줌)
+        System.out.println("id : " + id);
+        System.out.println("password : " + requestUser.getPassword());
+        System.out.println("email : " + requestUser.getEmail());
+        // form으로 안받고 json으로 받을 때는 @RequestBody를 사용
+
+        // update 할 때 사용 유저를 먼저 찾기
+        User user = userRepository.findById(id).orElseThrow(()->{
+            // 없다면?
+            return new IllegalArgumentException("수정에 실패하였습니다.");
+        });
+        // 있으면? 유저 객체 데이터 변경
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+        // userRepository에 user를 save
+//        userRepository.save(user);
+        // save 함수는 id를 전달하지 않으면 insert 해주고
+        // save 함수는 id를 전달하면 해당 id에 대한 데이터가 있을 때 update, 없을 때 insert를 한다.
+
+        // 더티 체킹
+
+
+        return null;
+    }
+
+    @GetMapping("/dummy/users")  // http://localhost:8096/myblog/dummy/users
+    public List<User> list(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/dummy/user")    // 한 페이지당 2건의 데이터를 리턴받아 볼 예정
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
+        Page<User> pagingUser = userRepository.findAll(pageable);
+        // 밑에 나오는 total page 수 이런 거 보기 싫으면?
+        List<User> users = pagingUser.getContent();
+        return users;
+    }
 
     @GetMapping("/dummy/user/{id}") // {id} -> 주소로 파라미터를 전달 받을 수 있음
     public User detail(@PathVariable long id){
@@ -55,4 +100,5 @@ public class DummyCotrollerTest {
         userRepository.save(user);
         return "회원가입이 완료되었습니다.";
     }
+
 }
