21e8ff3d3d679e8f13a454d08c139156461a3e3e
yenniesong <songye93@naver.com>
2024-03-03 04:25:34 +0900

    23th commit 카카오 로그인 서비스 구현2 완 이메일 가져울 수 있게 변경 완 (image 화면에 출력할 수 있게 수정 필요!)

--- src/main/java/com/study/myblog/controller/UserController.java
+++ src/main/java/com/study/myblog/controller/UserController.java
@@ -5,19 +5,35 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.study.myblog.model.KakaoProfile;
 import com.study.myblog.model.OAuthToken;
+import com.study.myblog.model.User;
+import com.study.myblog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UserController {
+    @Value("${yeyeh.key}")
+    private String yeyehKey;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     // 인증이 안된 사용자들이 출입할 수 있는 경로를 /auth/** 허용
     // 그냥 주소가 / 이면 index.jsp 허용
     // static 이하에 있는 /js/**, /css/**, /image/**
@@ -30,7 +46,7 @@ public class UserController {
         return "user/loginForm";
     }
     @GetMapping("/auth/kakao/callback")
-    public @ResponseBody String kakaoCallback(String code) throws JsonProcessingException {    // Data를 리턴해주는 컨트롤러 함수
+    public String kakaoCallback(String code) {    // Data를 리턴해주는 컨트롤러 함수
         // POST 방식으로 key = value 데이터를 요청 (카카오쪽으로)
         // Retrofit2
         // OkHttp
@@ -101,12 +117,37 @@ public class UserController {
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        System.out.println("카카오 아이디(번호) : " + kakaoProfile.getId());
-        System.out.println("카카오 아이디(번호) : " + kakaoProfile.getId());
-        System.out.println("카카오 아이디(번호) : " + kakaoProfile.getId());
 
+        // User 오브젝트 : username, password, email
+        System.out.println("카카오 아이디(번호) : " + kakaoProfile.getId());
+        System.out.println("카카오 이메일 : " + kakaoProfile.getKakao_account().getEmail());
 
-        return response2.getBody();
+        System.out.println("블로그 서버 유저네임 : " + kakaoProfile.getKakao_account().getEmail() + "-" + kakaoProfile.getId());
+        System.out.println("블로그 서버 이메일 : " + kakaoProfile.getKakao_account().getEmail());
+        // UUID 란? -> 중복되지 않는 어떤 특정 값을 만들어내는 알고리즘
+        System.out.println("블로그 서버 패스워드 : " + yeyehKey);
+
+        User kakaoUser = User.builder()
+                .username(kakaoProfile.getKakao_account().getEmail() + "-" + kakaoProfile.getId())
+                .password(yeyehKey)
+                .email(kakaoProfile.getKakao_account().getEmail())
+                .oauth("kakao")
+                .build();
+        // 가입자 혹은 비가입자 체크 해서 처리
+        User originUser = userService.회원찾기(kakaoUser.getUsername());
+
+        if (originUser.getUsername() == null) {
+            System.out.println("기존 회원이 아닙니다.");
+            userService.회원가입(kakaoUser);
+        }
+
+        // 로그인 처리
+        System.out.println("자동 로그인을 진행합니다.");
+        // 로그인 처리
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(kakaoUser.getUsername(), yeyehKey));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return "redirect:/";
     }
     @GetMapping("/user/updateForm")
     public String updateForm(){
--- src/main/java/com/study/myblog/model/User.java
+++ src/main/java/com/study/myblog/model/User.java
@@ -19,7 +19,7 @@ public class User {
     @Id // Primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
     private long id; // 시퀀스, auto_increment
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 100, unique = true)
     private String username; // 아이디
     @Column(length = 100)// 넉넉하게 주는 이유는 -> 해시로 변경해서 비밀번호를 암호화 할 것이라서
     private String password;
@@ -31,6 +31,8 @@ public class User {
     @Enumerated(EnumType.STRING)    // 해당 타입이 String인 것을 알려준다.
     private RoleType role;    // enum을 쓰는게 좋음. 도메인 설정을 할 수 있게 해줌.
     // 타입을 RoleType으로 해주면 강제적으로 USER, ADMIN만 나오게 한다.
+
+    private String oauth;   // kakao, google
 
     @CreationTimestamp  // 시간이 자동으로 입력된다.
     private Timestamp createDate;
